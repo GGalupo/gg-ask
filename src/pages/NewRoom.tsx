17c91abd5c3e63c3671cfa59aae88f5ce4a95f48
Gustavo Galupo <ggalupo2@hotmail.com>
2021-06-24 00:06:40 -0300

    Firebase auth + auth context

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
 
+// import { useAuth } from "../hooks/useAuth";
+
 import { Button } from "../components/Button";
 
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
-import googleIconImg from "../assets/images/google-icon.svg";
 import "../styles/auth.scss";
 
 export function NewRoom() {
+  // const { user } = useAuth();
+
   return (
     <div id="page-auth">
       <aside>
@@ -19,7 +22,6 @@ export function NewRoom() {
         <div className="main-content">
           <img src={logoImg} alt="ggask logo" />
           <h2>Create a new room</h2>
-
           <form action="">
             <input type="text" placeholder="Room name" />
             <Button type="submit">Create room</Button>
